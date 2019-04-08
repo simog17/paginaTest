aa194ede74cc83bcfabbf62f5c8e4b861033cc07
Buk00 <tortorella00@gmail.com>
2019-04-08 17:57:28 +0200

    added paginator

--- scripts.js
+++ scripts.js
@@ -1,41 +1,32 @@
 const content = document.getElementById('content')
-fetch("https://fakerestapi.azurewebsites.net/api/Books")
-.then(res => res.json())
-.then(data => content.innerHTML = data.slice(0 , 10).reduce((acc, e) => {
-  acc += `<article><h2><span> ${e.Title} </span></h2><p> ${e.Description} </p> </article>`
-  return acc
-}, "")
-)
-.then()
-.catch(err => {console.error(err)})
+const pagination = document.getElementById('pagination');
+const itemForPage = 10;
 
-
-const paginator = () => {
-
-}
-
-
-
-
-
-
-
-
-
-
-
+fetch('https://fakerestapi.azurewebsites.net/api/Books')
+  .then(res => res.json())
+  .then(data => {
+    const htmlPages = data.reduce((acc, {Title, Description}, index) => {
+      if(index % itemForPage === 0) {
+        acc.push([])
+      }
+      acc[acc.length - 1].push(`<article><h2><span> ${Title} </span></h2><p> ${Description} </p></article>`);
+      return acc;
+    }, []);
+    console.log(htmlPages)
+    content.innerHTML = htmlPages.shift().join('');
+    pagination.onclick = () => {
+      if(htmlPages.length > 0) {
+        content.innerHTML += htmlPages.shift().join('');
+      }
+    }
+  })
+  .catch(console.error);
 
 
 
 
 
+content.innerHTML = "<h1>Refresh</h1>"
 
 
 
-
-
-
-
-
-
-content.innerHTML = "<h1>Refresh</h1>" 
